38b7fa9c93b7c3a3cae2fd34d6eebcdfdc4b8ebb
Garrett Hardy <garrettmbh@gmail.com>
2018-11-14 11:46:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+
+
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+	var testRequest = new XMLHttpRequest();
+	//testRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+	testRequest.open('GET', 'test.json');
+	testRequest.onload = function() {
+		var testData = JSON.parse(testRequest.responseText);
+		renderHTML(testData);
+	};
+	testRequest.send();
+});
+
+function renderHTML(data) {
+	animalContainer.insertAdjacentHTML('beforeend', 'testing 123');
+}
+
+$('#btn').on('click', function() {
+	
+	$.ajax({
+		type: 'GET',
+		url: 'https://learnwebcode.github.io/json-example/animals-1.json',
+		success: function(animals) {
+			$.each(animals, function(i, animal) {
+				console.log(animal.name);
+			});
+		}
+	});
+});
+
+$('#putBut').on('click', function() {
+	
+	var dataObject = {
+		"name": "Sammy",
+		"species" : "frog",
+		"foods": {
+		  "likes": ["tuna", "catnip"],
+		  "dislikes": ["ham", "zucchini"]
+		}
+	};
+
+        alert(JSON.stringify(dataObject));
+
+        $.ajax({
+            url: URL,
+            type: 'POST',    
+            data: JSON.stringify(dataObject),
+            contentType: 'application/json',
+            success: function(result) {
+                alert("success?");
+            }
+        });
+});
+
